0dec1e1ab67ff80c05de9f63a092df36ab0a1821
TimelyManner <jslee@ijunsang-ui-MacBook-Pro.local>
2017-02-18 10:58:35 +0900

    init

---
+++ test/3d_model.py
@@ -0,0 +1,17 @@
+'''
+Created on 2017. 2. 18.
+
+@author: jslee
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
---
+++ test/__init__.py
@@ -0,0 +1,14 @@
+import os
+
+print(os.environ['PYTHONPATH'])
+print 'hhh'
+
+from direct.showbase.ShowBase import ShowBase
+ 
+class MyApp(ShowBase):
+ 
+    def __init__(self):
+        ShowBase.__init__(self)
+ 
+app = MyApp()
+app.run()
